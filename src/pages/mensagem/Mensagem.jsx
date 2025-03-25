e25a45ff8a32bfcf2e00f9529f2e4ed7a2e6db7a
Hélio Carlitos <heliocarlitosantonio@gmail.com>
2025-03-25 07:04:05 -0700

    Adicionei a lista de contactos e novos usuários

--- src/pages/mensagem/Mensagem.jsx
+++ src/pages/mensagem/Mensagem.jsx
@@ -1,11 +1,40 @@
 import { Helmet } from "react-helmet";
+import { Link } from "react-router-dom";
+import { CiSearch, CiMenuKebab } from "react-icons/ci";
+import { TfiDropbox } from "react-icons/tfi";
+import { useState, useRef, useEffect } from "react";
 import "./Mensagem.css";
 import TituloDaPagina from "../../components/titulodapagina/TituloDaPagina";
 import user_1 from "../../assets/image/usuarios/user_1.webp";
-import { CiSearch, CiMenuKebab } from "react-icons/ci";
-import { TfiDropbox } from "react-icons/tfi";
+import user_2 from "../../assets/image/usuarios/user_2.webp";
+import user_3 from "../../assets/image/usuarios/user_3.webp";
+import user_4 from "../../assets/image/usuarios/user_4.webp";
+import user_5 from "../../assets/image/usuarios/user_5.webp";
+import user_6 from "../../assets/image/usuarios/user_6.webp";
+import user_7 from "../../assets/image/usuarios/user_7.webp";
+import user_8 from "../../assets/image/usuarios/user_8.webp";
+import user_9 from "../../assets/image/usuarios/user_9.webp";
+import user_10 from "../../assets/image/usuarios/user_10.webp";
 
 function Mensagem() {
+    const [isOpen, setIsOpen] = useState(false);
+    const menuRef = useRef(null);
+
+    // Alternar visibilidade da lista
+    const toggleList = () => setIsOpen(!isOpen);
+
+    // Fecha a lista ao clicar fora
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (menuRef.current && !menuRef.current.contains(event.target)) {
+                setIsOpen(false);
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => document.removeEventListener("mousedown", handleClickOutside);
+    }, []);
+
     return (
         <>
             <Helmet>
@@ -13,17 +42,12 @@ function Mensagem() {
             </Helmet>
 
             <section className="Mensagem CardDefault">
-
                 <div className="Campo">
-
                     <div className="Bloco">
-
                         <TituloDaPagina Titulo="Mensagens" />
 
                         <div className="Usuario">
-
                             <div className="FotDetalhes">
-
                                 <figure>
                                     <img src={user_1} loading="lazy" alt="Foto de perfil" width={50} height={50} />
                                 </figure>
@@ -33,27 +57,20 @@ function Mensagem() {
                                     <p className="Cod-Usuario"> 08.0791.2017 </p>
                                     <p className="StatusOn"> Online </p>
                                 </div>
-
                             </div>
 
                             <div className="Opcoes">
-
                                 <button className="Icon">
                                     <CiMenuKebab />
                                 </button>
-
                             </div>
-
                         </div>
 
                         <div className="Pesquisa">
-
                             <div className="Icon">
                                 <CiSearch />
                             </div>
-
                             <input type="text" placeholder="Pesquisar" />
-
                         </div>
 
                         <div className="Men-Recentes">
@@ -61,34 +78,133 @@ function Mensagem() {
                         </div>
 
                         <div className="AsMensagens">
-
                             <p className="Caixa-vazia">
-
                                 Caixa vazia
-
                                 <div className="Icon">
                                     <TfiDropbox />
                                 </div>
-
                             </p>
-
                         </div>
 
-                        <div className="BtnNovaMsg">
-                            <button>
-                                +
-                            </button>
+                        {/* Botão + e Lista de Opções */}
+                        <div className="BtnNovaMsg" ref={menuRef}>
+                            <button onClick={toggleList}>+</button>
+
+                            {isOpen && (
+                                <div className="ListaNovaMsg">
+
+                                    <div className="Pesquisa">
+                                        <div className="Icon">
+                                            <CiSearch />
+                                        </div>
+                                        <input type="text" placeholder="Pesquisar" />
+                                    </div>
+
+                                    <nav>
+
+                                        <Link to="#">
+                                            <figure className="On TemStatus">
+                                                <img src={user_2} alt="Foto de perfil" width={40} height={40} loading="lazy" />
+                                            </figure>
+                                            <div className="Info">
+                                                <p className="Nome"> Carla Luís </p>
+                                                <p className="Codigo"> 08.0935.2017 </p>
+                                            </div>
+                                        </Link>
+
+                                        <Link to="#">
+                                            <figure className="TemStatus">
+                                                <img src={user_3} alt="Foto de perfil" width={40} height={40} loading="lazy" />
+                                            </figure>
+                                            <div className="Info">
+                                                <p className="Nome"> Éric Piter </p>
+                                                <p className="Codigo"> 08.7004.2017 </p>
+                                            </div>
+                                        </Link>
+
+                                        <Link to="#">
+                                            <figure className="TemStatus">
+                                                <img src={user_4} alt="Foto de perfil" width={40} height={40} loading="lazy" />
+                                            </figure>
+                                            <div className="Info">
+                                                <p className="Nome"> Sónia Veloso </p>
+                                                <p className="Codigo"> 08.1202.2017 </p>
+                                            </div>
+                                        </Link>
+
+                                        <Link to="#">
+                                            <figure className="On">
+                                                <img src={user_5} alt="Foto de perfil" width={40} height={40} loading="lazy" />
+                                            </figure>
+                                            <div className="Info">
+                                                <p className="Nome"> Carlos Lopes </p>
+                                                <p className="Codigo"> 08.1115.2017 </p>
+                                            </div>
+                                        </Link>
+
+                                        <Link to="#">
+                                            <figure>
+                                                <img src={user_6} alt="Foto de perfil" width={40} height={40} loading="lazy" />
+                                            </figure>
+                                            <div className="Info">
+                                                <p className="Nome"> José Ramos </p>
+                                                <p className="Codigo"> 08.0640.2017 </p>
+                                            </div>
+                                        </Link>
+
+                                        <Link to="#">
+                                            <figure>
+                                                <img src={user_7} alt="Foto de perfil" width={40} height={40} loading="lazy" />
+                                            </figure>
+                                            <div className="Info">
+                                                <p className="Nome"> Hélder Pei </p>
+                                                <p className="Codigo"> 08.5356.2017 </p>
+                                            </div>
+                                        </Link>
+
+                                        <Link to="#">
+                                            <figure>
+                                                <img src={user_8} alt="Foto de perfil" width={40} height={40} loading="lazy" />
+                                            </figure>
+                                            <div className="Info">
+                                                <p className="Nome"> Delfino Fujaro </p>
+                                                <p className="Codigo"> 08.9806.2017 </p>
+                                            </div>
+                                        </Link>
+
+                                        <Link to="#">
+                                            <figure>
+                                                <img src={user_9} alt="Foto de perfil" width={40} height={40} loading="lazy" />
+                                            </figure>
+                                            <div className="Info">
+                                                <p className="Nome"> Kelven António </p>
+                                                <p className="Codigo"> 08.1213.2017 </p>
+                                            </div>
+                                        </Link>
+
+                                        <Link to="#">
+                                            <figure className="On TemStatus">
+                                                <img src={user_10} alt="Foto de perfil" width={40} height={40} loading="lazy" />
+                                            </figure>
+                                            <div className="Info">
+                                                <p className="Nome"> Teresa Yuran </p>
+                                                <p className="Codigo"> 08.1213.2017 </p>
+                                            </div>
+                                        </Link>
+
+                                    </nav>
+
+                                </div>
+                            )}
                         </div>
-
                     </div>
 
-                    <div className="Conversas">
-
-                    </div>
+                    <div className="Conversas"></div>
+                    
                 </div>
-
             </section>
         </>
-    )
+    );
 }
+
 export default Mensagem;
